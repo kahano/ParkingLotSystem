5011ab06c6f46f5198a9e7eb382a9275e383933a
Mohamed Ali <kahano243@gmail.com>
2025-06-10 12:02:58 +0200

    Refactoring in Main class

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,8 @@ public class Main {
     public static void main(String[] args) {
 
         ParkLot parkLot = new ParkLot(80,"Helsfyr parking",5);
-        List<ParkingSpot> spots;
         ActiveParking activeParking = new ActiveParking(parkLot);
         Generate_Active_Tickets activeTickets = new Generate_Active_Tickets(activeParking);
-
-        spots = parkLot.getParkSpots();
         activeTickets.parkingMenu();
         System.out.println("\n ");
 
